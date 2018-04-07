8e6e022a1d4fdae04ee31cd667814643300d0bce
Belmin <belminf@gmail.com>
2018-04-07 19:52:35 -0400

    created setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='plex-delete',
+    version='0.1',
+    description='CLI to manage Plex media',
+    author='Belmin Fernandez',
+    author_email='belminf@gmail.com',
+    url='https://github.com/belminf/plex-delete',
+    scripts=['plex-delete'],
+)
